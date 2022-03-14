904c7aecec01e6eecf057c3022b9ce85bbadad81
will Koller <william.koller@invillia.com>
2022-03-14 14:16:51 -0300

    feat: array.at

---
+++ array-at.js
@@ -0,0 +1,9 @@
+const array = ['xbox', 'playstation', 'nintendo'];
+
+const xbox = array.at(0);
+const playstation = array.at(1);
+
+console.log(xbox);
+console.log(playstation);
+console.log(array.at(-1));
+console.log(array.at(-2));
